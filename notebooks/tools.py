16f070905e17402877b363e9ac2699846fd7387a
vaughanlove <vaughanlove1@gmail.com>
2023-09-06 14:46:21 -0600

    custom agent

--- notebooks/tools.py
+++ notebooks/tools.py
@@ -5,15 +5,22 @@ from langchain.callbacks.manager import (
 )
 import json
 from typing import Optional, Type
-    
-class GetLocationIdTool(BaseTool):
-    name="get_location_id_tool"
-    description="helpful for retrieving the location ID."
-    def _run(
-          self, run_manager: Optional[CallbackManagerForToolRun] = None
-      ) -> str:
-          """Right now this is hard coded."""
-          return "LETE3KSW0H1RB"
+
+from square.client import Client
+from square.api.catalog_api import CatalogApi
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+client = Client(
+    square_version='2023-08-16',
+    access_token=API_KEY,
+    environment='sandbox')
+
+# note everything in here will in theory be able to be called by a end user.
 
 class GetLocationNameTool(BaseTool):
     name="get_location_name_tool"
@@ -27,11 +34,39 @@ class GetLocationNameTool(BaseTool):
 
 class GetMenuTool(BaseTool):
     name="get_menu_tool"
-    description="helpful for getting all the menu items with prices"
+    description="helpful for getting the menu items"
     def _run(
-          self, run_manager: Optional[CallbackManagerForToolRun] = None
-      ) -> str:
-          """Test values"""
-          return {"coffee": 4, "tea": 2, "danish": 3}
+        self, args, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Get from square api"""
+        result = client.catalog.list_catalog(types = "ITEM" )
+        if result.is_success():
+            menu = []
+            for object in result.body['objects']:
+                for variation in object['item_data']['variations']:
+                    menu.append({
+                        'type' : object['item_data']['name'],
+                        'modifiers' : variation['item_variation_data']['name'].split(','),
+                        'item_id' : variation['id'],
+                        'price' : variation['item_variation_data']['price_money']['amount']                    
+                    })
+                return menu
+        elif result.is_error():
+          # todo error handling.
+          pass
+        return []
+
+
+class GetItemId(BaseTool):
+    name="get_item_id"
+    description="helpful for retrieving a specific item's id for ordering"
+    def _run(
+        self, item_name: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        menu = GetMenuTool()
+        return menu[item_name]['variations']
+        
+         
+         
 
 
