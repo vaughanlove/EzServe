1a514a79542dad48c69f60a64da9404f192e5ba9
Dominic Vandekerkhove <79351650+domvan@users.noreply.github.com>
2023-09-18 19:32:03 -0600

    Added audio client

--- app/source/audio/audio_client.py
+++ app/source/audio/audio_client.py
@@ -1,23 +1,37 @@
 """Records and Transcribes audio."""
 
-# import transcriber
-# import recorder
-
+import os
+from recorder import Recorder
+from transcriber import Transcriber
 
 class AudioClient(object):
     """Used by autoserve for handling audio input and transcription."""
+    def __init__(self, trace=False, verbose=True) -> bool:
+        #ensure google credentials are set (may have to use google project number for ID)
+        assert os.getenv("GOOGLE_PROJECT_ID") is not None, "MISSING GOOGLE_PROJECT_ID"
+        assert os.getenv("API_KEY") is not None, "API_KEY"
 
-    def listen(self):
+    def listen(self) -> object:
         """Record audio until user inputted stop.
         
         Returns (str):  Transcript 
         """
         # record audio until user inputted aync stop
+        input("Press enter to start recording")
+        recorder = Recorder()
+        audio = recorder.record()
 
-        # audio = recorder.record()
-        # transcriber.transcribe(audio)
+        print("Transcribing...")
+        transcriber = Transcriber()
+        transcript = transcriber.transcribe()
 
-        # do error checking/validation?
-        pass
+        print("Final transcription: " +transcript)
 
+        return transcript
 
+def main():
+    client = AudioClient()
+    client.listen()
+
+if __name__ == "__main__":
+    main()
--- app/source/audio/recorder.py
+++ app/source/audio/recorder.py
@@ -1,2 +1,53 @@
 """Recorder module for recording audio and stopped asyncronously"""
 
+import pyaudio
+import wave
+import keyboard
+
+class Recorder(object):
+    WAVE_OUTPUT_FILENAME = "tmp/audio_file.wav"
+
+    def __init__(self, trace=False, verbose=True) -> bool:
+        self.p = pyaudio.PyAudio()
+
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.CHUNK = 1024
+        self.RECORD_SECONDS = 10
+
+    def record(self):
+        """opens audio stream and writes to wav file till stopped"""
+        print("Opening stream")
+
+        stream = self.p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            frames_per_buffer=self.CHUNK,
+        )
+        frames = []
+
+        
+        #for _ in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)): #record for RECORD_SECONDS
+        # Record audio
+        #keyboard.is_pressed("r"): #doesnt work on mac, better to use than input()
+        print("Recording... (Press r to stop)")
+        while not input() == "r": #mac keyboard work-around
+            data = stream.read(self.CHUNK)
+            frames.append(data)
+
+        # Close and terminate the stream
+        stream.stop_stream()
+        stream.close()
+        self.p.terminate()
+
+        # Save the audio data as a .wav file
+        with wave.open(self.WAVE_OUTPUT_FILENAME, "wb") as wf:
+            wf.setnchannels(self.CHANNELS)
+            wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+            wf.setframerate(self.RATE)
+            wf.writeframes(b"".join(frames))
+
+        return
