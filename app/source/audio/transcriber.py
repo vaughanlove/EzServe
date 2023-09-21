ddde1ee2b292e7e3e5d9fb2ce698014c7e071d60
Vaughan <vaughanlove1@gmail.com>
2023-09-21 15:41:46 -0600

    moved record function into autoserve

--- app/source/audio/transcriber.py
+++ app/source/audio/transcriber.py
@@ -1,18 +1,22 @@
 """Transcriber module for transcribing wav files via Google api."""
 import os
-import wave
 from google.api_core.client_options import ClientOptions
 from google.cloud import aiplatform
 from google.cloud.speech_v2 import SpeechClient
 from google.cloud.speech_v2.types import cloud_speech
 
 class Transcriber(object):
+    """
+    Transcriber object to transcribe text from audio.
+    """
     WAVE_INPUT_FILENAME = "app/tmp/audio_file.wav"
 
     def __init__(self, trace=False, verbose=True) -> bool:
+
         google_project_id = os.getenv(
             "GOOGLE_PROJECT_ID"
         )  # ie, confident-jackle-123456
+        
         aiplatform.init(project=google_project_id, location="us-central1")
 
     def transcribe(self) -> object:
@@ -42,7 +46,7 @@ class Transcriber(object):
         # Transcribes the audio into text
         response = client.recognize(request=request)
 
-        if response.results[0] != None:
+        if response.results[0] is not None:
             print("TRANSCRIPTION: " + response.results[0].alternatives[0].transcript)
             return response.results[0].alternatives[0].transcript
         else:
--- app/source/autoserve.py
+++ app/source/autoserve.py
@@ -1,5 +1,8 @@
 """ AutoServe module for combining the transcription module with the LLM agent."""
 
+from source.audio.translator import Translator
+from source.audio.transcriber import Transcriber
+from source.agent.square_client import SquareClient
 from source.agent.tools import (
     OrderTool,
     GetDetailedMenuTool,
@@ -8,72 +11,101 @@ from source.agent.tools import (
     GetOrderTool,
 )
 
-from source.audio.recorder import Recorder
-from source.audio.transcriber import Transcriber
-
+import pyaudio
+import wave
+import asyncio
+import re
 import os
-from enum import Enum
 
 from langchain.agents import AgentType, initialize_agent
 from langchain.llms import vertexai
 
 from google.cloud import aiplatform
-from google.cloud.speech_v2 import SpeechClient
-from google.cloud.speech_v2.types import cloud_speech
+
 
 from dotenv import load_dotenv
 
-import asyncio
-
-stop_recording = asyncio.Event() 
-
-# want to start with it stopped.
-
-class State(Enum):
-    RECORDING = 1
-    PROCESSING = 2
-    WAITING = 3
+recording_flag = asyncio.Event()
+processing_flag = asyncio.Event()
+shutdown_flag = asyncio.Event()
 
 class AutoServe:
     """For managing state."""
-    state = State.WAITING
-
     history = []
     transcribed_string = None
-    WAVE_OUTPUT_FILENAME = "app/tmp/audio_file.wav"
 
+    WAVE_OUTPUT_FILENAME = "app/tmp/audio_file.wav"
     agent = None
     llm = None
-    tools = [
-        GetDetailedMenuTool(),
-        FindItemIdTool(),
-        OrderTool(),
-        MakeOrderCheckoutTool(),
-        GetOrderTool(),
-    ]
+    
+    async def run(self):
+        # Start both tasks and run them concurrently
+        record_task = asyncio.create_task(self.record())
+        input_task = asyncio.create_task(self.handle_input())
 
-    def __init__(self, trace=False, verbose=True) -> bool:
-        self._load_env_vars(trace)
-        self._setup_gcloud()
+        await input_task
+        await record_task
+        
+    async def record(self):
+        """opens audio stream and writes to wav file till stopped"""
 
-        self.recorder = Recorder(trace=trace, verbose=verbose)
-        self.transcriber = Transcriber()
+        while True:
+            
+            while not recording_flag.is_set():
+                await asyncio.sleep(0.05)
 
-        self.llm = vertexai.VertexAI(temperature=0)
+            p = pyaudio.PyAudio()
 
-        assert self.llm is not None, "LLM NOT INSTANTIATED"
-        assert len(self.tools) > 0, "NEED AT LEAST ONE TOOL"
+            stream = p.open(
+                format=self.format,
+                channels=self.channels,
+                rate=self.rate,
+                input=True,
+                frames_per_buffer=self.chunk,
+            )
+    
+            frames = []
 
-        self.agent = initialize_agent(
-            self.tools,
-            self.llm,
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            verbose=verbose,
-        )
+            while recording_flag.is_set(): #mac keyboard work-around
+                data = stream.read(self.chunk)
+                frames.append(data)
+
+                await asyncio.sleep(1 / 44100)  # This is hack but 1/ hz = seconds/sample and she works.
+
+            # Close and terminate the stream
+            stream.stop_stream()
+            stream.close()
+            sample_width = p.get_sample_size(self.format)
+            p.terminate()
+
+            # Save the audio data as a .wav file
+            with wave.open(self.WAVE_OUTPUT_FILENAME, "wb") as wf:
+                wf.setnchannels(self.channels)
+                wf.setsampwidth(sample_width)
+                wf.setframerate(self.rate)
+                wf.writeframes(b"".join(frames))
+                wf.close()
+
+            recording_flag.clear()
+            processing_flag.set()
+
+            # do the transcription
+            transcript = self.t.transcribe()
+
+            result = self.translator.translate(transcript)
+            
+            clean_result = re.sub(r'[^a-zA-Z0-9\s]', '', result)
+
+            #print("translation: " + result)
+            #print("clean translation: " + clean_result)
+
+            agent_response = self.agent.run(clean_result)
+            print("AGENT: " + agent_response)
+
+            processing_flag.clear()
 
     async def handle_input(self):
-        print(f"""
-              Currently {self.state}. 
+        print("""
               Press r to record.
               Press s to stop recording.
               """)
@@ -81,33 +113,24 @@ class AutoServe:
         while True:
             user_input = await asyncio.to_thread(input, "")  # Take user input asynchronously
             if user_input == 'r':
-                if self.state == State.RECORDING:
+
+                if recording_flag.is_set():
                     print("Already recording!")
-                else:
+
+                elif processing_flag.is_set():
+                    print("Hold on, still processing the last message.")
+                
+                elif not processing_flag.is_set() and not recording_flag.is_set():
                     print("Starting the recording.")
-                    self.state=State.RECORDING
-                    stop_recording.clear()
+                    recording_flag.set()
 
             elif user_input == 's':
-                if self.state != State.RECORDING:
-                    print("Not recording!")
-                elif self.state == State.RECORDING:
-                    print("Stopping the recording.")
-                    stop_recording.set()
-                    self.state = State.WAITING
+                if recording_flag.is_set():
+                    print("Stopping recording!")
+                    recording_flag.clear()
 
-
-    async def run(self):
-        stop_recording.set()
-        # Start both tasks and run them concurrently
-        record_task = asyncio.create_task(self.recorder.record(stop_recording))
-        input_task = asyncio.create_task(self.handle_input())
-
-        await input_task
-        await record_task
-
-    def get_state(self) -> State:
-        return self.state
+                elif processing_flag.is_set():
+                    print("Recording is already stopped. Please wait for the last message to be processed.")
 
     def _load_env_vars(self, trace: bool) -> None:
         # load environment variables from .env file
@@ -136,3 +159,41 @@ class AutoServe:
             "GOOGLE_PROJECT_ID"
         )  # ie, confident-jackle-123456
         aiplatform.init(project=google_project_id, location="us-central1")
+
+    def __init__(self, trace=False, verbose=True) -> bool:
+        self._load_env_vars(trace)
+        self._setup_gcloud()
+
+        tools = [
+            GetDetailedMenuTool(),
+            FindItemIdTool(),
+            OrderTool(),
+            MakeOrderCheckoutTool(),
+            GetOrderTool(),
+        ]
+
+        self.transcriber = Transcriber()
+
+        self.llm = vertexai.VertexAI(temperature=0)
+
+        assert self.llm is not None, "LLM NOT INSTANTIATED"
+        assert len(self.tools) > 0, "NEED AT LEAST ONE TOOL"
+
+        # pyaudio settings
+        self.format = pyaudio.paInt16
+        self.channels = 1
+        self.rate = 44100
+        self.chunk = 1024
+
+        self.recording = True
+
+        self.t = Transcriber()
+        self.translator = Translator()
+        self.agent = SquareClient(trace=trace, verbose=verbose)
+
+        self.agent = initialize_agent(
+            tools,
+            self.llm,
+            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=verbose,
+        )
